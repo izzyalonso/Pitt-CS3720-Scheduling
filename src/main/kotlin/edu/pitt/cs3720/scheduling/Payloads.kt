4eeb1c9b5ee3e4221629970ad333f68b08695650
Ismael Alonso <ismaha91@gmail.com>
2019-10-12 20:12:18 -0400

    Add reactions to some events in the scheduler

--- src/main/kotlin/edu/pitt/cs3720/scheduling/Payloads.kt
+++ src/main/kotlin/edu/pitt/cs3720/scheduling/Payloads.kt
@@ -4,13 +4,21 @@ import edu.pitt.cs3720.scheduling.framework.Event
 import edu.pitt.cs3720.scheduling.framework.Payload
 
 
+// To notify the scheduler a device came online
 class DeviceOnline(val device: Device): Payload
+// To notify the scheduler a device went offline
 class DeviceOffline(val device: Device): Payload
+// A reboot event; device specific
 class Awake: Payload
+// A work request from the scheduler to a device
 class WorkRequest(val job: Job): Payload
+// A work completed notification from a device to the scheduler
 class WorkCompleted(val device:Device, val job: Job): Payload
+// For the scheduler to periodically check on scheduled jobs
 class Timeout(val device: Device, val job: Job): Payload
+// From the scheduler to know what the status of a device is
 class StatusRequest: Payload
+// A response to a status request
 class StatusUpdate(status: Status): Payload
 
 
--- src/main/kotlin/edu/pitt/cs3720/scheduling/Scheduler.kt
+++ src/main/kotlin/edu/pitt/cs3720/scheduling/Scheduler.kt
@@ -1,5 +1,6 @@
 package edu.pitt.cs3720.scheduling
 
+import edu.pitt.cs3720.scheduling.framework.Controller
 import edu.pitt.cs3720.scheduling.framework.Event
 import edu.pitt.cs3720.scheduling.framework.EventListener
 
@@ -11,8 +12,14 @@ abstract class Scheduler(private val jobs: MutableList<Job>, private val timeout
 
     override fun onEvent(event: Event) {
         event.deviceOnline()?.let { deviceOnline ->
-            schedule.put(deviceOnline.device, null)
-            // TODO schedule work on this device
+            if (schedule.containsKey(deviceOnline.device)) {
+                // The device was already registered; it failed and recovered before we could notice
+                schedule.remove(deviceOnline.device)?.let { job ->
+                    // If it had a job assigned, throw it back to the pool
+                    jobs.add(job)
+                }
+            }
+            scheduleWorkOn(deviceOnline.device, event.time)
         }
         event.deviceOffline()?.let { deviceOffline ->
             // Remove a device from the registry and return its job to the lake
@@ -20,8 +27,35 @@ abstract class Scheduler(private val jobs: MutableList<Job>, private val timeout
                 jobs.add(job)
             }
         }
+        event.workCompleted()?.let { workCompleted ->
+            scheduleWorkOn(workCompleted.device, event.time)
+        }
         event.timeout()?.let { timeout ->
-
+            /**Controller.addEvent(Event(
+                time = event.time + 50,
+                payload = StatusRequest(),
+                listener = timeout.device
+            ))*/
         }
     }
+
+    private fun scheduleWorkOn(device: Device, currentTime: Int) {
+        // Schedule the work
+        val nextJob = nextJobFor(device)
+        schedule[device] = nextJob
+        Controller.addEvent(Event(
+            time = currentTime + 50,
+            payload = WorkRequest(nextJob),
+            listener = device
+        ))
+
+        // And set up a timeout
+        Controller.addEvent(Event(
+            time = currentTime + timeoutMillis,
+            payload = Timeout(device = device, job = nextJob),
+            listener = this
+        ))
+    }
+
+    abstract fun nextJobFor(device: Device): Job
 }
