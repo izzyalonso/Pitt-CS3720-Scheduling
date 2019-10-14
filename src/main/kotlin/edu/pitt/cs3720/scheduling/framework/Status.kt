f10b611a5d0119bf8f6e4571e500ed624c744676
Ismael Alonso <ismaha91@gmail.com>
2019-10-14 17:38:47 -0400

    Add a to string to status

--- src/main/kotlin/edu/pitt/cs3720/scheduling/framework/Status.kt
+++ src/main/kotlin/edu/pitt/cs3720/scheduling/framework/Status.kt
@@ -1,4 +1,6 @@
 package edu.pitt.cs3720.scheduling.framework
 
 
-data class Status(val working: Boolean)
+data class Status(val working: Boolean) {
+    override fun toString() = if (working) "working" else "idle"
+}
